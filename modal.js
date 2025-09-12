e828ac0f974db5b42dd27cec1877017506b95eb0
mulsivera <adrien.dasilva.ferreira@gmail.com>
2025-09-12 08:38:37 +0200

    Ajout de la fonctionnalité de fermeture de la modale au clic de l'icôn X

--- modal.js
+++ modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.getElementById("close-button")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// Close modal event
+closeBtn.addEventListener("click", CloseModal);
+
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//Close modal form
+function CloseModal() {
+  modalbg.style.display = "None";
 }
 
 
